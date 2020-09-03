2a2a651b2d7d3cf3b82e5f6b24375bfcd1736a42
Nick <yux50000@hotmail.com>
2020-09-02 21:20:40 -0400

    More work on build process..

--- PostBuild.pas
+++ PostBuild.pas
@@ -16,20 +16,53 @@
   simultaneously.
 }
 
-{$ifdef UNIX}uses BaseUnix;{$endif}
-{$ifdef WINDOWS}uses Windows;{$endif}
+{$mode objfpc}{$H+}
 
-procedure Mklink(Src, Dst: String);
+uses SysUtils,
+
+{$ifdef UNIX}BaseUnix;{$endif}
+{$ifdef WINDOWS}Windows;{$endif}
+
+// Cringe
+{$ifdef WINDOWS}
+const
+  SYMBOLIC_LINK_FLAG_DIRECTORY = $00000001;
+
+function CreateSymbolicLinkA(
+  lpSymlinkFileName: PAnsiChar;
+  lpTargetFileName: PAnsiChar;
+  dwFlags: DWORD
+): Boolean; stdcall; external 'kernel32';
+{$endif}
+
+procedure Mklink(Src, Dst: AnsiString; Dir: Boolean = False);
 begin
+  writeln(src, ', ', dst, ', ', dir);
   {$ifdef UNIX}
   // TODO....
   {$endif}
 
   {$ifdef WINDOWS}
-    // Todo...
+    if Dir then
+      writeln('link dir ', CreateSymbolicLinkA(PAnsiChar(Dst), PAnsiChar(Src), SYMBOLIC_LINK_FLAG_DIRECTORY))
+    else
+      writeln('link file ', CreateSymbolicLinkA(PAnsiChar(Dst), PAnsiChar(Src), 0))
   {$endif}
 end;
 
+var
+  I: Integer;
+  Dev: Boolean;
+  DestDir: String;
 begin
+  for I := 0 to ParamCount-1 do writeln(Paramstr(I));
+  DestDir := ParamStr(2);
+  Dev := True; ///(ParamCount > 2) and (ParamStr(2) = '-d');
 
+  if Dev then begin
+    Mklink('hUGETracker', DestDir+'hUGETracker', True);
+    Mklink('Resources/ROMs/halt.gb', DestDir+'halt.gb');
+  end else begin
+    writeln('not implemented.');
+  end;
 end.
