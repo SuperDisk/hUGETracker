f0b36e628c53314836d60821e4520021402aa7c5
Nick <yux50000@hotmail.com>
2020-10-29 00:45:53 -0400

    WIP codegen changes, remove unused files

--- codegen.pas
+++ codegen.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes, SysUtils, Math, Instruments, Song, Utils,
   HugeDatatypes, Constants, Dialogs, strutils, FileUtil, LazFileUtils,
-  lclintf, process, rgb2sdas;
+  lclintf, process;
 
 type
   TExportMode = (emNormal, emPreview, emGBS);
@@ -192,186 +192,220 @@ begin
   OutSL.Free;
 end;
 
-function RenderOrderTable(OrderMatrix: TOrderMatrix): string;
+procedure RenderSongToHTTs(Song: TSong; DescriptorName: String; Filename: string);
+  function RenderOrderTable(OrderMatrix: TOrderMatrix): string;
 
-  function ArrayHelper(Ints: array of integer): string;
+    function ArrayHelper(Ints: array of integer): string;
+    var
+      I: integer;
+      SL: TStringList;
+    begin
+      SL := TStringList.Create;
+      SL.StrictDelimiter := True;
+      SL.Delimiter := ',';
+      for I := Low(Ints) to High(Ints)-1 do // HACK: account for the off-by-one error
+        SL.Add('P' + IntToStr(Ints[I]));
+      Result := SL.DelimitedText;
+      SL.Free;
+    end;
+
   var
-    I: integer;
+    Res: TStringList;
+    OrderCnt: integer;
+  begin
+    Res := TStringList.Create;
+    OrderCnt := MaxIntValue([High(OrderMatrix[0]), High(OrderMatrix[1]),
+      High(OrderMatrix[2]), High(OrderMatrix[3])]);
+
+    Res.Add('order_cnt: db ' + IntToStr(OrderCnt * 2));
+    Res.Add('order1: dw ' + ArrayHelper(OrderMatrix[0]));
+    Res.Add('order2: dw ' + ArrayHelper(OrderMatrix[1]));
+    Res.Add('order3: dw ' + ArrayHelper(OrderMatrix[2]));
+    Res.Add('order4: dw ' + ArrayHelper(OrderMatrix[3]));
+
+    Result := Res.Text;
+    Res.Free;
+  end;
+
+  function RenderInstruments(Instruments: TInstrumentBank): string;
+  var
+    SL, ResultSL: TStringList;
+    AsmInstrument: TAsmInstrument;
+    I, J: integer;
+    TypePrefix: string;
+    HighMask: byte;
+  begin
+    ResultSL := TStringList.Create;
+
+    for I := Low(Instruments) to High(Instruments) do
+    begin
+      AsmInstrument := InstrumentToBytes(Instruments[I]);
+      SL := TStringList.Create;
+      SL.StrictDelimiter := True;
+      SL.Delimiter := ',';
+
+      if Instruments[I].Type_ = itNoise then
+      begin
+        SL.Add(IntToStr(AsmInstrument[1])); // envelope
+
+        HighMask := AsmInstrument[0];
+        if Instruments[I].LengthEnabled then
+          HighMask := HighMask or %01000000;
+        if Instruments[I].CounterStep = swSeven then
+          HighMask := HighMask or %10000000;
+        SL.Add(IntToStr(HighMask));
+
+        for J := Low(TNoiseMacro) to High(TNoiseMacro) do
+          SL.Add(IntToStr(Instruments[I].NoiseMacro[J]));
+      end
+      else
+        for J := Low(AsmInstrument) to High(AsmInstrument) do
+          SL.Add(IntToStr(AsmInstrument[J]));
+
+      WriteStr(TypePrefix, Instruments[I].Type_);
+      ResultSL.Add(Format('%s%s: db %s', [TypePrefix, 'inst' + IntToStr(I),
+        SL.DelimitedText]));
+      SL.Free;
+    end;
+
+    Result := ResultSL.Text;
+    ResultSL.Free;
+  end;
+
+  function RenderCell(Cell: TCell): string;
+  var
     SL: TStringList;
   begin
     SL := TStringList.Create;
-    SL.StrictDelimiter := True;
     SL.Delimiter := ',';
-    for I := Low(Ints) to High(Ints)-1 do // HACK: account for the off-by-one error
-      SL.Add('P' + IntToStr(Ints[I]));
-    Result := SL.DelimitedText;
-    SL.Free;
-  end;
-
-var
-  Res: TStringList;
-  OrderCnt: integer;
-begin
-  Res := TStringList.Create;
-  OrderCnt := MaxIntValue([High(OrderMatrix[0]), High(OrderMatrix[1]),
-    High(OrderMatrix[2]), High(OrderMatrix[3])]);
-
-  Res.Add('order_cnt: db ' + IntToStr(OrderCnt * 2));
-  Res.Add('order1: dw ' + ArrayHelper(OrderMatrix[0]));
-  Res.Add('order2: dw ' + ArrayHelper(OrderMatrix[1]));
-  Res.Add('order3: dw ' + ArrayHelper(OrderMatrix[2]));
-  Res.Add('order4: dw ' + ArrayHelper(OrderMatrix[3]));
-
-  Result := Res.Text;
-  Res.Free;
-end;
-
-function RenderInstruments(Instruments: TInstrumentBank): string;
-var
-  SL, ResultSL: TStringList;
-  AsmInstrument: TAsmInstrument;
-  I, J: integer;
-  TypePrefix: string;
-  HighMask: byte;
-begin
-  ResultSL := TStringList.Create;
-
-  for I := Low(Instruments) to High(Instruments) do
-  begin
-    AsmInstrument := InstrumentToBytes(Instruments[I]);
-    SL := TStringList.Create;
     SL.StrictDelimiter := True;
-    SL.Delimiter := ',';
 
-    if Instruments[I].Type_ = itNoise then
-    begin
-      SL.Add(IntToStr(AsmInstrument[1])); // envelope
-
-      HighMask := AsmInstrument[0];
-      if Instruments[I].LengthEnabled then
-        HighMask := HighMask or %01000000;
-      if Instruments[I].CounterStep = swSeven then
-        HighMask := HighMask or %10000000;
-      SL.Add(IntToStr(HighMask));
-
-      for J := Low(TNoiseMacro) to High(TNoiseMacro) do
-        SL.Add(IntToStr(Instruments[I].NoiseMacro[J]));
-    end
+    if Cell.Note = NO_NOTE then
+      SL.Add('___')
     else
-      for J := Low(AsmInstrument) to High(AsmInstrument) do
-        SL.Add(IntToStr(AsmInstrument[J]));
+      SL.Add(NoteToDriverMap.KeyData[Cell.Note]);
 
-    WriteStr(TypePrefix, Instruments[I].Type_);
-    ResultSL.Add(Format('%s%s: db %s', [TypePrefix, 'inst' + IntToStr(I),
-      SL.DelimitedText]));
+    if InRange(Cell.Instrument, 0, 15) then
+      SL.Add(IntToStr(Cell.Instrument))
+    else
+      SL.Add('0');
+
+    SL.Add('$' + EffectCodeToStr(Cell.EffectCode, Cell.EffectParams));
+
+    // RGBDS thinks you're defining a new macro if you don't have a space first.
+    Result := ' dn ' + SL.DelimitedText;
     SL.Free;
   end;
 
-  Result := ResultSL.Text;
-  ResultSL.Free;
-end;
-
-function RenderCell(Cell: TCell): string;
-var
-  SL: TStringList;
-begin
-  SL := TStringList.Create;
-  SL.Delimiter := ',';
-  SL.StrictDelimiter := True;
-
-  if Cell.Note = NO_NOTE then
-    SL.Add('___')
-  else
-    SL.Add(NoteToDriverMap.KeyData[Cell.Note]);
-
-  if InRange(Cell.Instrument, 0, 15) then
-    SL.Add(IntToStr(Cell.Instrument))
-  else
-    SL.Add('0');
-
-  SL.Add('$' + EffectCodeToStr(Cell.EffectCode, Cell.EffectParams));
-
-  // RGBDS thinks you're defining a new macro if you don't have a space first.
-  Result := ' dn ' + SL.DelimitedText;
-  SL.Free;
-end;
-
-function RenderPattern(Name: string; Pattern: TPattern): string;
-var
-  SL: TStringList;
-  I: integer;
-begin
-  SL := TStringList.Create;
-  SL.Add(Name + ':');
-
-  for I := Low(TPattern) to High(TPattern) do
-    SL.Add(RenderCell(Pattern[I]));
-
-  Result := SL.Text;
-  SL.Free;
-end;
-
-function RenderWaveforms(Waves: TWaveBank): string;
-var
-  SL, ResultSL: TStringList;
-  I, J: integer;
-begin
-  ResultSL := TStringList.Create;
-
-  for I := Low(Waves) to High(Waves) do
+  function RenderPattern(Name: string; Pattern: TPattern): string;
+  var
+    SL: TStringList;
+    I: integer;
   begin
     SL := TStringList.Create;
-    SL.StrictDelimiter := True;
-    SL.Delimiter := ',';
+    SL.Add(Name + ':');
 
-    J := Low(Waves[I]);
-    while J < High(Waves[I]) do
-    begin
-      SL.Add(IntToStr((Waves[I, J] shl 4) or Waves[I, J + 1]));
-      Inc(J, 2);
-    end;
-    ResultSL.Add(Format('wave%d: db %s', [I, SL.DelimitedText]));
+    for I := Low(TPattern) to High(TPattern) do
+      SL.Add(RenderCell(Pattern[I]));
+
+    Result := SL.Text;
     SL.Free;
   end;
 
-  Result := ResultSL.Text;
-  ResultSL.Free;
-end;
-
-function RenderConstants(Song: TSong; Mode: TExportMode): string;
-var
-  SL: TStringList;
-begin
-  SL := TStringList.Create;
-  SL.Add('STANDALONE_MODE EQU 1');
-
-  if Mode = emPreview then
-    SL.Add('PREVIEW_MODE EQU 1')
-  else if Mode = emGBS then
+  function RenderWaveforms(Waves: TWaveBank): string;
+  var
+    SL, ResultSL: TStringList;
+    I, J: integer;
   begin
-    SL.Add('GBS_MODE EQU 1');
-    SL.Add('GBS_TITLE EQUS "\"' + PadRight(Song.Name, 32) + '\""');
-    SL.Add('GBS_AUTHOR EQUS "\"' + PadRight(Song.Artist, 32) + '\""');
-    SL.Add('GBS_COPYRIGHT EQUS "\"' + PadRight(IntToStr(CurrentYear), 32) + '\""');
+    ResultSL := TStringList.Create;
+
+    for I := Low(Waves) to High(Waves) do
+    begin
+      SL := TStringList.Create;
+      SL.StrictDelimiter := True;
+      SL.Delimiter := ',';
+
+      J := Low(Waves[I]);
+      while J < High(Waves[I]) do
+      begin
+        SL.Add(IntToStr((Waves[I, J] shl 4) or Waves[I, J + 1]));
+        Inc(J, 2);
+      end;
+      ResultSL.Add(Format('wave%d: db %s', [I, SL.DelimitedText]));
+      SL.Free;
+    end;
+
+    Result := ResultSL.Text;
+    ResultSL.Free;
   end;
-  SL.Add('TICKS EQU ' + IntToStr(Song.TicksPerRow));
 
-  Result := SL.Text;
-  SL.Free;
-end;
-
-procedure WriteRoutinesToFile(Routines: TRoutineBank);
-var
-  I: integer;
-  F: Text;
-begin
-  for I := Low(TRoutineBank) to High(TRoutineBank) do
+  function RenderConstants(Song: TSong; Mode: TExportMode): string;
+  var
+    SL: TStringList;
   begin
-    AssignFile(F, './hUGEDriver/routine' + IntToStr(I) + '.htt');
-    Rewrite(F);
-    Write(F, Routines[I]);
-    CloseFile(F);
+    SL := TStringList.Create;
+    SL.Add('STANDALONE_MODE EQU 1');
+
+    if Mode = emPreview then
+      SL.Add('PREVIEW_MODE EQU 1')
+    else if Mode = emGBS then
+    begin
+      SL.Add('GBS_MODE EQU 1');
+      SL.Add('GBS_TITLE EQUS "\"' + PadRight(Song.Name, 32) + '\""');
+      SL.Add('GBS_AUTHOR EQUS "\"' + PadRight(Song.Artist, 32) + '\""');
+      SL.Add('GBS_COPYRIGHT EQUS "\"' + PadRight(IntToStr(CurrentYear), 32) + '\""');
+    end;
+    SL.Add('TICKS EQU ' + IntToStr(Song.TicksPerRow));
+
+    Result := SL.Text;
+    SL.Free;
   end;
+
+  procedure WriteRoutinesToFile(Routines: TRoutineBank);
+  var
+    I: integer;
+    F: Text;
+  begin
+    for I := Low(TRoutineBank) to High(TRoutineBank) do
+    begin
+      AssignFile(F, './hUGEDriver/routine' + IntToStr(I) + '.htt');
+      Rewrite(F);
+      Write(F, Routines[I]);
+      CloseFile(F);
+    end;
+  end;
+
+  procedure WriteHTT(F: string; S: string);
+  begin
+    AssignFile(OutFile, F);
+    Rewrite(OutFile);
+    Write(OutFile, S);
+    CloseFile(OutFile);
+  end;
+begin
+  FilePath := Filename;
+  Filename := ExtractFileNameWithoutExt(ExtractFileNameOnly(Filename));
+
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_constants.htt', RenderConstants(Song, Mode));
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_wave.htt', RenderWaveforms(Song.Waves));
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_order.htt', RenderOrderTable(Song.OrderMatrix));
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_duty_instrument.htt', RenderInstruments(Song.Instruments.Duty));
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_wave_instrument.htt', RenderInstruments(Song.Instruments.Wave));
+  WriteHTT('./hUGEDriver/'+DescriptorName+'_noise_instrument.htt',
+    RenderInstruments(Song.Instruments.Noise));
+  WriteRoutinesToFile(Song.Routines);
+
+  AssignFile(OutFile, './hUGEDriver/pattern.htt');
+  Rewrite(OutFile);
+
+  // TODO: Are keys and data defined to be aligned? Seems like they are but
+  // should probably find out if that's just an implementation detail...
+  for I := 0 to Song.Patterns.Count - 1 do
+    if PatternIsUsed(Song.Patterns.Keys[I], Song) then
+      Write(OutFile, RenderPattern('P' + IntToStr(Song.Patterns.Keys[I]),
+        Song.Patterns.Data[I]^));
+
+  CloseFile(OutFile);
 end;
 
 procedure RectifyGBSFile(GBSFile: string);
@@ -409,14 +443,6 @@ var
   OutSL: TStringList;
   FilePath: string;
   RenameSucceeded: Boolean;
-
-  procedure WriteHTT(F: string; S: string);
-  begin
-    AssignFile(OutFile, F);
-    Rewrite(OutFile);
-    Write(OutFile, S);
-    CloseFile(OutFile);
-  end;
 
   function Assemble(OutFile: string; InFile: string; Defines: array of string): integer;
   var
@@ -469,29 +495,7 @@ label
 begin
   Song := OptimizeSong(Song);
 
-  FilePath := Filename;
-  Filename := ExtractFileNameWithoutExt(ExtractFileNameOnly(Filename));
-
-  WriteHTT('./hUGEDriver/constants.htt', RenderConstants(Song, Mode));
-  WriteHTT('./hUGEDriver/wave.htt', RenderWaveforms(Song.Waves));
-  WriteHTT('./hUGEDriver/order.htt', RenderOrderTable(Song.OrderMatrix));
-  WriteHTT('./hUGEDriver/duty_instrument.htt', RenderInstruments(Song.Instruments.Duty));
-  WriteHTT('./hUGEDriver/wave_instrument.htt', RenderInstruments(Song.Instruments.Wave));
-  WriteHTT('./hUGEDriver/noise_instrument.htt',
-    RenderInstruments(Song.Instruments.Noise));
-  WriteRoutinesToFile(Song.Routines);
-
-  AssignFile(OutFile, './hUGEDriver/pattern.htt');
-  Rewrite(OutFile);
-
-  // TODO: Are keys and data defined to be aligned? Seems like they are but
-  // should probably find out if that's just an implementation detail...
-  for I := 0 to Song.Patterns.Count - 1 do
-    if PatternIsUsed(Song.Patterns.Keys[I], Song) then
-      Write(OutFile, RenderPattern('P' + IntToStr(Song.Patterns.Keys[I]),
-        Song.Patterns.Data[I]^));
-
-  CloseFile(OutFile);
+  RenderSongToHTTs(Song, DescriptorName, Filename);
 
   // Build the file
   Chdir('hUGEDriver');
