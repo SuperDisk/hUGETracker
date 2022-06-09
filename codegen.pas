7a2268628c63befd9cc72d1527506d716740ed54
Nick Faro <yux50000@hotmail.com>
2022-06-08 21:37:31 -0400

    Clamp table jumps to 0-32

--- codegen.pas
+++ codegen.pas
@@ -343,7 +343,7 @@ begin
   if Last and (Cell.Volume = 0) then
     SL.Add(IntToStr(1)) // Automatically insert jump back to row 1 if last cell
   else
-    SL.Add(IntToStr(Cell.Volume));
+    SL.Add(IntToStr(EnsureRange(Cell.Volume, 0, 32)));
 
   SL.Add('$' + EffectCodeToStr(Cell.EffectCode, Cell.EffectParams));
 
