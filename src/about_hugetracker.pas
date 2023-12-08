0057d262bb0ad32f2edc2bd708aab5af4b1f1b4b
Nick Faro <yux50000@hotmail.com>
2023-12-07 20:11:15 -0500

    Add version string baked in during compile time

--- src/about_hugetracker.pas
+++ src/about_hugetracker.pas
@@ -18,8 +18,10 @@ type
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    VersionLabel: TLabel;
     Panel1: TPanel;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure FormDeactivate(Sender: TObject);
   private
 
@@ -39,6 +41,17 @@ implementation
 procedure TfrmAboutHugetracker.Button1Click(Sender: TObject);
 begin
   Close;
+end;
+
+procedure TfrmAboutHugetracker.FormCreate(Sender: TObject);
+var
+  GitVersion: String;
+begin
+  GitVersion := {$I %VERSION_STRING%};
+  if GitVersion = '' then
+    VersionLabel.Caption := 'Unknown Version'
+  else
+    VersionLabel.Caption := 'Version '+GitVersion;
 end;
 
 procedure TfrmAboutHugetracker.FormDeactivate(Sender: TObject);
