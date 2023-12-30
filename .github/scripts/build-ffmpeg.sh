42e96310188fb1edc1d2de7afecf075ef7d2ff07
Nick Faro <yux50000@hotmail.com>
2023-12-30 15:45:07 -0500

    Remove brew lame, maybe this will work

--- .github/scripts/build-ffmpeg.sh
+++ .github/scripts/build-ffmpeg.sh
@@ -8,10 +8,13 @@ set -x
 curl -Lo lame.tar.gz 'https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz'
 tar xf lame.tar.gz
 pushd lame-3.100
-./configure --prefix="`_realpath ..`/lame-install" --disable-dependency-tracking --enable-static --disable-shared --enable-nasm --disable-gtktest --disable-decoder --disable-frontend
+./configure --prefix="`_realpath ..`/lame-install" --disable-dependency-tracking \
+            --enable-static --disable-shared --enable-nasm --disable-gtktest \
+            --disable-decoder --disable-frontend
 make
 make install
 popd
+
 curl -Lo ffmpeg.tar.xz 'http://ffmpeg.org/releases/ffmpeg-4.4.tar.xz'
 tar xf ffmpeg.tar.xz
 cd ffmpeg-4.4
@@ -22,5 +25,5 @@ cd ffmpeg-4.4
             --enable-encoder=flac --enable-muxer=ogg --enable-muxer=flac --enable-encoder=pcm_s16le \
             --enable-muxer=wav --enable-libmp3lame --enable-encoder=libmp3lame --enable-muxer=mp3 \
             --extra-cflags="-I`_realpath ../lame-install/include`" \
-            --extra-ldflags="-L`_realpath ../lame-install/lib` -l:libmp3lame.a" || cat ffbuild/config.log
+            --extra-ldflags="-L`_realpath ../lame-install/lib`" || cat ffbuild/config.log
 make
