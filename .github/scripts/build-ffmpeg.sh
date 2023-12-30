2626de999deafdeb05d1d58e4c0e1c024f00a665
Nick Faro <yux50000@hotmail.com>
2023-12-30 15:31:56 -0500

    Remove wrong flags from ffmpeg, move to lame

--- .github/scripts/build-ffmpeg.sh
+++ .github/scripts/build-ffmpeg.sh
@@ -8,14 +8,14 @@ set -x
 curl -Lo lame.tar.gz 'https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz'
 tar xf lame.tar.gz
 pushd lame-3.100
-./configure --prefix="`_realpath ..`/lame-install" --disable-dependency-tracking --disable-shared --enable-nasm --disable-gtktest --disable-decoder --disable-frontend
+./configure --prefix="`_realpath ..`/lame-install" --disable-dependency-tracking --enable-static --disable-shared --enable-nasm --disable-gtktest --disable-decoder --disable-frontend
 make
 make install
 popd
 curl -Lo ffmpeg.tar.xz 'http://ffmpeg.org/releases/ffmpeg-4.4.tar.xz'
 tar xf ffmpeg.tar.xz
 cd ffmpeg-4.4
-./configure --disable-shared --enable-static  --pkg-config-flags=--static \
+./configure --pkg-config-flags=--static \
             --disable-debug --enable-lto --disable-programs --enable-ffmpeg \
             --disable-doc --disable-everything --enable-protocol=pipe --enable-protocol=file \
             --enable-filter=aresample --enable-decoder=pcm_f32le --enable-demuxer=pcm_f32le \
