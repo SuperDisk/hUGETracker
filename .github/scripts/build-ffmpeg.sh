c8ae502969b9f2caebeaec15d7b3db080fc6ce6f
Nick Faro <yux50000@hotmail.com>
2024-01-27 20:30:19 -0500

    Fix FFMpeg issue on windows. Remove old lazarus formula

--- .github/scripts/build-ffmpeg.sh
+++ .github/scripts/build-ffmpeg.sh
@@ -13,12 +13,19 @@ popd
 curl -Lo ffmpeg.tar.xz 'http://ffmpeg.org/releases/ffmpeg-4.4.tar.xz'
 tar xf ffmpeg.tar.xz
 cd ffmpeg-4.4
-./configure --pkg-config-flags=--static \
+
+if [ "$(uname)" = "Darwin" ]; then
+    STATIC_FLAG="--pkg-config-flags=--static"
+else
+    STATIC_FLAG="--extra-ldflags=-static"
+fi
+
+./configure $STATIC_FLAG \
             --disable-debug --enable-lto --disable-programs --enable-ffmpeg \
             --disable-doc --disable-everything --enable-protocol=pipe --enable-protocol=file \
             --enable-filter=aresample --enable-decoder=pcm_f32le --enable-demuxer=pcm_f32le \
             --enable-encoder=flac --enable-muxer=ogg --enable-muxer=flac --enable-encoder=pcm_s16le \
             --enable-muxer=wav --enable-libmp3lame --enable-encoder=libmp3lame --enable-muxer=mp3 \
             --extra-cflags="-I`realpath ../lame-install/include`" \
-            --extra-ldflags="-L`realpath ../lame-install/lib`" || cat ffbuild/config.log
+            --extra-ldflags="-L`realpath ../lame-install/lib`"
 make
